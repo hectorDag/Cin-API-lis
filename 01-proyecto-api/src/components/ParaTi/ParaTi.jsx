a2fe13efeb6eab17839df98ef1434c21c3fdb6ad
Osvaldo G Del Rio <osdelrio_0@hotmail.com>
2023-05-18 00:00:09 -0700

    Issues

--- 01-proyecto-api/src/components/ParaTi/ParaTi.jsx
+++ 01-proyecto-api/src/components/ParaTi/ParaTi.jsx
@@ -6,98 +6,50 @@ import '../ParaTi/ParaTi.css'
 const ParaTi = () => {
   const [movies, setMovies] = useState([])
   const [images, setImages] = useState([])
-  console.log(movies)
+
+  /**
+   * Un solo use effect para traer las peliculas y las imagenes, no es necesario hacer dos llamadas a la api, con una sola es suficiente
+   */
 
   useEffect(() => {
     fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=747b36d52c8c76c86925e8a5867acb43')
       .then(response => response.json())
-      .then(data => setImages(data.results.map(image => `https://image.tmdb.org/t/p/w500${image.poster_path}`)))
+      .then(data => {
+        setMovies(data.results);
+        setImages(data.results.map(image => `https://image.tmdb.org/t/p/w500${image.poster_path}`))
+      })
       .catch(error => console.log(error))
   }, [])
 
-  useEffect(() => {
-    fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=747b36d52c8c76c86925e8a5867acb43')
-      .then(response => response.json())
-      .then(data => setMovies(data.results))
-      .catch(error => console.log(error))
-  }, [])
+  // dividimos el array de peliculas en chunks de 5 para mostrar 5 peliculas por slide
+  const movieChunks = movies && Array(Math.ceil(movies.length / 5)).fill().map((_, index) => index * 5).map(begin => movies.slice(begin, begin + 5));
 
+  /**
+   * En el return, en vez de hacer un carousel item por cada imagen, hacemos un carousel item por cada chunk de peliculas, 
+   * y dentro de cada carousel item hacemos un map de las peliculas de ese chunk.
+   * De esta forma, si cambiamos el numero de peliculas por slide, no tenemos que cambiar el codigo, solo el css
+   */
   return (
-
     <>
-
       <h1>Top Rated</h1>
 
       <Carousel className='carousel-card'>
-
-        <Carousel.Item>
-          <div className='cards-wrapper'>
-
-            <Card className='individual-card' style={{ width: '18rem' }}>
-              <Card.Img variant='top' src={images[0]} />
-              <Card.Body>
-                <p>{movies[1].title}</p>
-              </Card.Body>
-            </Card>
-
-            <Card className='individual-card' style={{ width: '18rem' }}>
-              <Card.Img variant='top' src={images[1]} />
-              <Card.Body />
-            </Card>
-
-            <Card className='individual-card' style={{ width: '18rem' }}>
-              <Card.Img variant='top' src={images[2]} />
-              <Card.Body />
-            </Card>
-
-            <Card className='individual-card' style={{ width: '18rem' }}>
-              <Card.Img variant='top' src={images[3]} />
-              <Card.Body />
-            </Card>
-
-            <Card className='individual-card' style={{ width: '18rem' }}>
-              <Card.Img variant='top' src={images[4]} />
-              <Card.Body />
-            </Card>
-
-          </div>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <div className='cards-wrapper'>
-
-            <Card className='individual-card' style={{ width: '18rem' }}>
-              <Card.Img variant='top' src={images[5]} />
-              <Card.Body />
-            </Card>
-
-            <Card className='individual-card' style={{ width: '18rem' }}>
-              <Card.Img variant='top' src={images[6]} />
-              <Card.Body />
-            </Card>
-
-            <Card className='individual-card' style={{ width: '18rem' }}>
-              <Card.Img variant='top' src={images[7]} />
-              <Card.Body />
-            </Card>
-
-            <Card className='individual-card' style={{ width: '18rem' }}>
-              <Card.Img variant='top' src={images[8]} />
-              <Card.Body />
-            </Card>
-
-            <Card className='individual-card' style={{ width: '18rem' }}>
-              <Card.Img variant='top' src={images[9]} />
-              <Card.Body />
-            </Card>
-
-          </div>
-        </Carousel.Item>
-
+        {movieChunks && movieChunks.map((movieChunk, chunkIndex) => (
+          <Carousel.Item key={chunkIndex}>
+            <div className='cards-wrapper'>
+              {movieChunk.map((movie, index) => (
+                <Card key={index} className='individual-card' style={{ width: '20%' }}>
+                  <Card.Img variant='top' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+                  <Card.Body>
+                    <p>{movie.title}</p>
+                  </Card.Body>
+                </Card>
+              ))}
+            </div>
+          </Carousel.Item>
+        ))}
       </Carousel>
-
     </>
-
   )
 }
 export default ParaTi
