8abbeea3ca6a3b8e4a9922c6dcaaadd925e871d5
David Alvarez <david.alvgar23@gmail.com>
2023-05-17 21:54:08 -0700

    Rebicion

--- 01-proyecto-api/src/components/ParaTi/ParaTi.jsx
+++ 01-proyecto-api/src/components/ParaTi/ParaTi.jsx
@@ -6,20 +6,20 @@ import '../ParaTi/ParaTi.css'
 const ParaTi = () => {
   const [movies, setMovies] = useState([])
   const [images, setImages] = useState([])
+  console.log(movies)
 
   useEffect(() => {
     fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=747b36d52c8c76c86925e8a5867acb43')
       .then(response => response.json())
       .then(data => setImages(data.results.map(image => `https://image.tmdb.org/t/p/w500${image.poster_path}`)))
-      .then(data => setMovies(data.results))
-      .catch(error => console.error(error))
+      .catch(error => console.log(error))
   }, [])
 
   useEffect(() => {
     fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=747b36d52c8c76c86925e8a5867acb43')
       .then(response => response.json())
       .then(data => setMovies(data.results))
-      .catch(error => console.error(error))
+      .catch(error => console.log(error))
   }, [])
 
   return (
@@ -36,36 +36,28 @@ const ParaTi = () => {
             <Card className='individual-card' style={{ width: '18rem' }}>
               <Card.Img variant='top' src={images[0]} />
               <Card.Body>
-                <Card.Title>{movies[0].title}</Card.Title>
+                <p>{movies[1].title}</p>
               </Card.Body>
             </Card>
 
             <Card className='individual-card' style={{ width: '18rem' }}>
               <Card.Img variant='top' src={images[1]} />
-              <Card.Body>
-                <Card.Title>{movies[1].title}</Card.Title>
-              </Card.Body>
+              <Card.Body />
             </Card>
 
             <Card className='individual-card' style={{ width: '18rem' }}>
               <Card.Img variant='top' src={images[2]} />
-              <Card.Body>
-                <Card.Title>{movies[2].title}</Card.Title>
-              </Card.Body>
+              <Card.Body />
             </Card>
 
             <Card className='individual-card' style={{ width: '18rem' }}>
               <Card.Img variant='top' src={images[3]} />
-              <Card.Body>
-                <Card.Title>{movies[3].title}</Card.Title>
-              </Card.Body>
+              <Card.Body />
             </Card>
 
             <Card className='individual-card' style={{ width: '18rem' }}>
               <Card.Img variant='top' src={images[4]} />
-              <Card.Body>
-                <Card.Title>{movies[4].title}</Card.Title>
-              </Card.Body>
+              <Card.Body />
             </Card>
 
           </div>
@@ -76,37 +68,27 @@ const ParaTi = () => {
 
             <Card className='individual-card' style={{ width: '18rem' }}>
               <Card.Img variant='top' src={images[5]} />
-              <Card.Body>
-                <Card.Title className='card-title'>{movies[5].title}</Card.Title>
-              </Card.Body>
+              <Card.Body />
             </Card>
 
             <Card className='individual-card' style={{ width: '18rem' }}>
               <Card.Img variant='top' src={images[6]} />
-              <Card.Body>
-                <Card.Title>{movies[6].title}</Card.Title>
-              </Card.Body>
+              <Card.Body />
             </Card>
 
             <Card className='individual-card' style={{ width: '18rem' }}>
               <Card.Img variant='top' src={images[7]} />
-              <Card.Body>
-                <Card.Title>{movies[7].title}</Card.Title>
-              </Card.Body>
+              <Card.Body />
             </Card>
 
             <Card className='individual-card' style={{ width: '18rem' }}>
               <Card.Img variant='top' src={images[8]} />
-              <Card.Body>
-                <Card.Title>{movies[8].title}</Card.Title>
-              </Card.Body>
+              <Card.Body />
             </Card>
 
             <Card className='individual-card' style={{ width: '18rem' }}>
               <Card.Img variant='top' src={images[9]} />
-              <Card.Body>
-                <Card.Title>{movies[9].title}</Card.Title>
-              </Card.Body>
+              <Card.Body />
             </Card>
 
           </div>
